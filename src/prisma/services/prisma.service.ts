34070db7a087c0a7e50139d57b54b8232445c05b
fpelizza <ferminpelizza1976@gmail.com>
2026-03-15 20:21:55 -0300

    Implement retry logic for database connection in PrismaService to enhance reliability on startup

--- src/prisma/services/prisma.service.ts
+++ src/prisma/services/prisma.service.ts
@@ -4,6 +4,17 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit(): Promise<void> {
-    await this.$connect();
+    let retries = 5;
+
+    while (retries) {
+      try {
+        await this.$connect();
+        break;
+      } catch (error) {
+        retries--;
+        console.log('Retrying database connection...');
+        await new Promise((res) => setTimeout(res, 5000));
+      }
+    }
   }
 }
